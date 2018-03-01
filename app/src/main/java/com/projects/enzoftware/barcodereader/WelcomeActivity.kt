2d9c4435efb4c389e4d20303e9ff3247ae88dde2
enzoftware <lizama.enzo@gmail.com>
2018-02-28 23:04:56 -0500

    Animated welcome screen

--- app/src/main/java/com/projects/enzoftware/barcodereader/WelcomeActivity.kt
+++ app/src/main/java/com/projects/enzoftware/barcodereader/WelcomeActivity.kt
@@ -1,12 +1,28 @@
 package com.projects.enzoftware.barcodereader
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import kotlinx.android.synthetic.main.activity_welcome.*
 
 class WelcomeActivity : AppCompatActivity() {
+
+    private lateinit var upToDown: Animation
+    private lateinit var downToUp : Animation
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+        upToDown = AnimationUtils.loadAnimation(this,R.anim.up_to_down)
+        downToUp = AnimationUtils.loadAnimation(this,R.anim.down_to_up)
+        linearLayoutSuperior.animation = upToDown
+        linearLayoutInferior.animation = downToUp
+        buttonStart.setOnClickListener {
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        }
+
     }
 }
