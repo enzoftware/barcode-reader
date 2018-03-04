5c1460400a0dcd00c36f9418dbb68df43d8dd4ba
enzoftware <lizama.enzo@gmail.com>
2018-03-04 15:48:10 -0500

    Finishing recyclerview implementation

--- app/src/main/java/com/projects/enzoftware/barcodereader/adapter/RecyclerViewAdapter.kt
+++ app/src/main/java/com/projects/enzoftware/barcodereader/adapter/RecyclerViewAdapter.kt
@@ -2,8 +2,12 @@ package com.projects.enzoftware.barcodereader.adapter
 
 import android.content.Context
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import com.projects.enzoftware.barcodereader.R
 import com.projects.enzoftware.barcodereader.model.Barcode
+import kotlinx.android.synthetic.main.card_view_barcode.view.*
 
 /**
  * Created by enzoftware on 3/4/18.
@@ -12,15 +16,23 @@ import com.projects.enzoftware.barcodereader.model.Barcode
 
 class RecyclerViewAdapter (var context: Context, var list: ArrayList<Barcode>): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder?, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        (holder as Item).bindData(list[position],context)
     }
 
     override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return list.size
     }
 
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): RecyclerView.ViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val v = LayoutInflater.from(context).inflate(R.layout.card_view_barcode,parent,false)
+        return Item(v)
+    }
+
+    class Item(itemView:View): RecyclerView.ViewHolder(itemView){
+        fun bindData(_list: Barcode, context: Context){
+            itemView.barcode_id.text =_list.id
+            itemView.barcode_code.text = _list.code
+        }
     }
 
 }
--- app/src/main/java/com/projects/enzoftware/barcodereader/model/Barcode.kt
+++ app/src/main/java/com/projects/enzoftware/barcodereader/model/Barcode.kt
@@ -4,4 +4,4 @@ package com.projects.enzoftware.barcodereader.model
  * Created by enzoftware on 3/4/18.
  */
 
-data class Barcode(val id:String, val code:String)
+data class Barcode(var id:String, var code:String)
