6ff8f1009163aff0583a14f1fc3995b8173717d3
enzoftware <lizama.enzo@gmail.com>
2018-03-06 21:38:40 -0500

    Adding icons

--- app/src/main/java/com/projects/enzoftware/barcodereader/ListActivity.kt
+++ app/src/main/java/com/projects/enzoftware/barcodereader/ListActivity.kt
@@ -4,17 +4,12 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
-import android.view.View
 import com.projects.enzoftware.barcodereader.adapter.RecyclerViewAdapter
 import com.projects.enzoftware.barcodereader.model.Barcode
 import com.projects.enzoftware.barcodereader.utils.cleanDB
 import com.projects.enzoftware.barcodereader.utils.readFromDB
-import kotlinx.android.synthetic.main.app_bar.*
 import kotlinx.android.synthetic.main.app_bar_list.*
-import org.jetbrains.anko.alert
-import org.jetbrains.anko.noButton
 import org.jetbrains.anko.toast
-import org.jetbrains.anko.yesButton
 
 class ListActivity : AppCompatActivity() {
     private var barcode_list : ArrayList<Barcode> = ArrayList()
